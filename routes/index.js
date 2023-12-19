faa3359265a37aad0b2508757da59275d3305116
Agasthya Udupa <agasthyauduparajkumar2001@gmail.com>
2023-12-19 19:18:53 +0530

    edit-profile-updated

--- routes/index.js
+++ routes/index.js
@@ -15,11 +15,11 @@ router.get('/login', function (req, res) {
   res.render('login', { footer: false });
 });
 
-router.get('/feed', isLoggedIn,function (req, res) {
+router.get('/feed', isLoggedIn, function (req, res) {
   res.render('feed', { footer: true });
 });
 
-router.get('/profile',  isLoggedIn,function (req, res) {
+router.get('/profile', isLoggedIn, function (req, res) {
   res.render('profile', { footer: true });
 });
 
@@ -27,12 +27,13 @@ router.get('/search', isLoggedIn, function (req, res) {
   res.render('search', { footer: true });
 });
 
-router.get('/edit',  isLoggedIn,function (req, res) {
+router.get('/edit', isLoggedIn, async function (req, res) {
+  const user = await userModel.findOne({ username: req.session.passport.user })
   res.render('edit', { footer: true });
 });
 
 router.get('/upload', isLoggedIn, function (req, res) {
-  res.render('upload', { footer: true });
+  res.render('upload', { footer: true,user });
 });
 
 router.post('/register', function (req, res, next) {
@@ -56,18 +57,25 @@ router.post('/login', passport.authenticate('local', {
 router.post('/logout', function (req, res, next) {
   req.logout(function (err) {
     if (err) {
-      return next(err)}
-      res.redirect('/')
+      return next(err)
+    }
+    res.redirect('/')
   })
 });
 
-router.post('/update',function(req,res){
-
+router.post('/update', upload.single('image'), async function (req, res) {
+  const user = await userModel.findOneAndUpdate({ username: req.session.passport.user },
+    { username: req.body.username, name: req.body.name, bio: req.body.bio },
+    { new: true })
+  user.profileImage = req.file.filename
+  await user.save()
+  res.redirect('/profile')
 })
 
-function isLoggedIn(req,res,next){
-  if(req.isAuthenticated()) return next()
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next()
   res.redirect('/login')
 }
 
-module.exports = router;
+module.exports = router
